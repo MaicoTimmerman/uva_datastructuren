53c788c4006e639ad804bc56e6d8450e63ad3739
Maico Timmerman <maico.timmerman@gmail.com>
2014-02-04 22:01:55 +0100

    Fine tuning

--- assignment1/pi-approx.c
+++ assignment1/pi-approx.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+/* For INT_MIN and INT_MAX */
 #include <limits.h>
+/* For sqrt() */
 #include <math.h>
+/* For time() */
 #include <time.h>
 
 #define ARGS_REQUIRED 2
