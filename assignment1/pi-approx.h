cc7f37a17f329e43aa95990d36e1289c7da09b71
Maico Timmerman <maico.timmerman@gmail.com>
2014-02-05 16:59:21 +0100

    Added headerfile

---
+++ assignment1/pi-approx.h
@@ -0,0 +1,13 @@
+#ifndef __PI_APPROX_H__
+#define __PI_APPROX_H__
+
+/*
+ * Function: help()
+ * ----------------
+ * Print the usage of this program
+ *
+ * @returns succes after printing the help of this program
+ */
+int help();
+
+#endif 
