813388e89f1be583602f635f6dd26027bf53bb9a
Maico Timmerman <maico.timmerman@gmail.com>
2014-03-04 21:11:50 +0100

    Added assignment 4

---
+++ assignment4/hash.h
@@ -0,0 +1,10 @@
+typedef struct hash_t {
+    *table;
+};
+
+hash_t* hash_table_new(unsigned int size);
+void hash_table_destroy(hash_t* table);
+void hash_table_insert(hash_t* table, void* key, void* value);
+void* hash_table_lookup(hash_t* table, void* key);
+unsigned int hash_table_size(hash_t* table);
+unsigned int hash_table_fill(hash_t* table);
